757fb33584202fd03c385b878d397c54fadacc2b
kawlevinit <kawlevinit@gmail.com>
2021-03-05 16:18:12 +0530

    First commit

---
+++ audiobook.py
@@ -0,0 +1,11 @@
+from gtts import gTTS
+import os
+
+f=open('audiobook.txt')
+x=f.read()
+langugage = 'hin'
+
+audio =gTTS(text=x,lang=langugage)
+
+audio.save('audiobook.wav')
+os.system('audiobook.wav')
